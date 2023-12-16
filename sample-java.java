f4647b4806dbd5f7f3f445fff02029a553297a6d
Fayaz <smfayaz@yahoo.com>
2023-12-16 10:17:22 +0530

    fayaz-second commit --fayaz2

--- sample-java.java
+++ sample-java.java
@@ -4,8 +4,11 @@
 class HelloWorld { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
+    
+	public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World");
+	System.out.println("WELCOME");
+
     } 
 }
